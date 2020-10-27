047929f654ee6f118f41042f1576a3cb497b702a
Michal Kuspit <michal.kuspit@gmail.com>
2020-10-27 19:18:35 +0400

    Update s-shape transformation

--- mmminion.py
+++ mmminion.py
@@ -10,12 +10,8 @@ def s_shape(x, a, b):
     Returns an S-shaped curve Y = (X ^ a) / ((X ^ a) + (b ^ a))
     a interpretation - higher 'a' means steeper growth
     b interpretation - f(b) = 1/2
-    For a = b = 0 returns original value
     """
-    if a == 0 and b == 0:
-        return x
-    else:
-        return ((x ** a) / ((x ** a) + (b ** a)))
+    return ((x ** a) / ((x ** a) + (b ** a)))
 
 
 def dim_returns(x, a):
